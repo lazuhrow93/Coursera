28e9f870940f01b558613dbca07d430d6c2ba350
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2018-02-17 00:12:57 -0600

    Updating binary_search from Lazaro laptop

--- binary_search.cpp
+++ binary_search.cpp
@@ -6,53 +6,34 @@ using std::vector;
 
 int binary_search(const vector<int> &a, int x) {
   int left = 0, right = (int)a.size(); 
-  //vector<int> newVector1;
-  int pivot = (right-left)/2;
-  bool found = false;
+  //Write your Code here
+  int pivot = 0;
+  pivot = pivot + (right-left)/2;
 
-
-  while(!found){
-  	
-  	if(pivot < 0 || pivot > a.size()){ //if its out of range, 
-  		return - 1;
-  	}
-  	else if(x == a[pivot]){
-  		return pivot;
-  	}
-  	else if(x < a[pivot]){
-  		right = pivot-1;
-  	}
-  	else{
-  		left = pivot + 1;
-  	}
+  if(x < a[0] || x > a[right-1]){
+    //std::cout<<x<<" is not in range\n"<<a[0]<<":"<<a[right-1];
+    return -1; //since sorted, you can check right away if its not in the array
   }
-  
 
-  /*
-  if(right == 0){
-  	return -1;
+  while(true){
+    
+    if(x == a[pivot]){
+      return pivot;
+    }
+    else if(left >= right){ //if left == right, that means we search the sorted array accordingly, and still didnt find the answer.
+      return -1;
+    }
+    else if(x > a[pivot]){
+      left = pivot+1;
+      pivot = left + ((right-left)/2);
+
+    }
+    else{//if x < a[pivot]
+      right = pivot-1;
+      pivot = left + ((right - left)/2);
+    }
+
   }
-  else if(a[pivot] == x){
-  	return pivot;
-  }
-  else if(x > a[pivot]){
-
-  	for(int i = pivot + 1; i < right; i++) newVector1.push_back(a[i]);
-
-  	if(binary_search(newVector1, x) == -1) return -1;
-  	else return pivot + binary_search(newVector1 ,x) + 1;
- 
-  }
-  else{
-
-  	for(int i = left; i < pivot; i++) newVector1.push_back(a[i]); 
-
-  	return binary_search(newVector1, x);
-  }
-  */
-
-
-  //write your code here
 }
 
 int linear_search(const vector<int> &a, int x) {
@@ -72,20 +53,14 @@ int main() {
   int m;
   std::cin >> m;
   vector<int> b(m);
+  
+  
   for (int i = 0; i < m; ++i) {
     std::cin >> b[i];
   }
-  /*
+  
   for (int i = 0; i < m; ++i) {
     //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
+    std::cout << binary_search(a, b[i]) << ' ';
   }
-	*/
-  //std::cout <<"\n";
-
-  for (int i = 0; i < m; ++i){
-  	std::cout << binary_search(a, b[i]) << ' ';
-  }
-
 }
-

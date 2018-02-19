f7c9596371cf92cb3d78b4d090541705e1486cd9
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2018-02-18 19:15:01 -0600

    binary_search.cpp done! changed int to int64_t

--- BinarySearch/binary_search.cpp
+++ BinarySearch/binary_search.cpp
@@ -4,10 +4,10 @@
 
 using std::vector;
 
-int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
+int64_t binary_search(const vector<int64_t> &a, int64_t x) {
+  int64_t left = 0, right = (int64_t)a.size(); 
   //Write your Code here
-  int pivot = 0;
+  int64_t pivot = 0;
   pivot = pivot + (right-left)/2;
 
   if(x < a[0] || x > a[right-1]){
@@ -20,7 +20,7 @@ int binary_search(const vector<int> &a, int x) {
     if(x == a[pivot]){
       return pivot;
     }
-    else if(left >= right){ //if left == right, that means we search the sorted array accordingly, and still didnt find the answer.
+    else if(left > right){ //if left == right, that means we search the sorted array accordingly, and still didnt find the answer.
       return -1;
     }
     else if(x > a[pivot]){
@@ -44,22 +44,22 @@ int linear_search(const vector<int> &a, int x) {
 }
 
 int main() {
-  int n;
+  int64_t n;
   std::cin >> n;
-  vector<int> a(n);
+  vector<int64_t> a(n);
   for (size_t i = 0; i < a.size(); i++) {
     std::cin >> a[i];
   }
-  int m;
+  int64_t m;
   std::cin >> m;
-  vector<int> b(m);
+  vector<int64_t> b(m);
   
   
-  for (int i = 0; i < m; ++i) {
+  for (int64_t i = 0; i < m; ++i) {
     std::cin >> b[i];
   }
   
-  for (int i = 0; i < m; ++i) {
+  for (int64_t i = 0; i < m; ++i) {
     //replace with the call to binary_search when implemented
     std::cout << binary_search(a, b[i]) << ' ';
   }

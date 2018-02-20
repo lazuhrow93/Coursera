6ddbc9cf5ffbc43a02eb340687b3c9a0702a108a
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2018-02-19 20:04:47 -0600

    removed output messages

--- MajorityElement/MajorityElement.cpp
+++ MajorityElement/MajorityElement.cpp
@@ -3,14 +3,12 @@
 #include <vector>
 
 using std::vector;
-int debug = 1;
 
 int partition(vector<int> &a, int left, int right){
 
   //you comparison element
   int pivot = a[left]; //pivot
   int j = left;
-
 
   for (int i = left+1; i < right; ++i){
     if(a[i] <= pivot){ //if this element is less than the pivot, 
@@ -27,22 +25,10 @@ int partition(vector<int> &a, int left, int right){
 
 void quickSort(vector<int> &a, int l, int r){
   int mid = 0;
-  //std::cout<<"\n";
+
   if(l >= r) return;
 
-  if(debug){
-    for(int k = l; k<r; k++){
-      std::cout<<a[k];
-    }
-    std::cout<<"\n";
-  }
   mid = partition(a, l ,r);
-
-
-
-  if(debug){
-    std::cout<<"Here is mid: A["<<mid<<"]"<<" = "<<a[mid]<<std::endl;
-  }
 
   quickSort(a, l, mid-1);
   quickSort(a, mid+1, r);
@@ -54,20 +40,11 @@ int get_majority_element(vector<int> &a, int left, int right) {
   if (left == right) return -1; //only 1 element, no majority
   if (left + 1 == right) return a[left]; //if theyre only two elements, return arbitrary one 
 
-  
   /* we want to sort first, and then we can just iterate through the numbers
      to find the majority element */
   quickSort(a, left, right); //O(nlogn)
 
-  if(debug){
-    std::cout<<"Heres the resulting array\n";
-    for(int y = 0; y<a.size(); y++){
-      std::cout<<a[y]<<" ";
-    }  
-  }
-  
-  
-
+  //iterate through array to find a majority
   int current_interest = 0;
   int majority_test = 0; //count of specific number
   int majority_value = a.size()/2;
@@ -88,13 +65,8 @@ int get_majority_element(vector<int> &a, int left, int right) {
     if(majority_test > majority_value) return 1;
   }
 
-  //std::cout<<"\n";
   return -1;
 
-
-
-
-  //write your code here
  
 }
 

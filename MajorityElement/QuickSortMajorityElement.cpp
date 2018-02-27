16b95f0db7e1afa6d8752889e838ec0555b83750
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2018-02-26 18:54:36 -0600

    idk

--- MajorityElement/QuickSortMajorityElement.cpp
+++ MajorityElement/QuickSortMajorityElement.cpp
@@ -35,39 +35,6 @@ void quickSort(vector<int> &a, int l, int r){
 
 }
 
-int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1; //no elements
-  if (left + 1 == right) return a[left]; //only one element 
-
-  
-  /* we want to sort first, and then we can just iterate through the numbers
-     to find the majority element */
-  quickSort(a, left, right-1); //O(nlogn)
-  
-  //iterate through array to find a majority
-  int current_interest = 0;
-  int majority_test = 0; //count of specific number
-  int majority_value = a.size()/2;
-
-  //by the time we get here, we know there are at least two elements. 
-  current_interest = a[0];  
-
-  //now that its sorted, iterate and find if there is a majority 
-  for(int i = 0; i<a.size(); ++i){
-    if(a[i] == current_interest){
-      ++majority_test;
-    }
-    else{
-      current_interest = a[i];
-      majority_test = 1;
-    }
-
-    if(majority_test > majority_value) return 1;
-  }
-  return -1;
-
-}
-
 int main() {
   int n;
   std::cin >> n;
@@ -75,5 +42,10 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+
+  quickSort(a, 0, a.size()-1);
+
+  for(int x = 0; x<a.size(); x++){
+    std::cout<<"Sorted Array: "<<a[x]<<", ";
+  }
 }

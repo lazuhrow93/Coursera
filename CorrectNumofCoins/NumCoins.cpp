6c499be04d0b5553d3ff19399ab0370fe0ac3137
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2018-03-04 16:03:20 -0600

    Add a faster approach to the problem. Also added assert to help me debug

--- CorrectNumofCoins/NumCoins.cpp
+++ CorrectNumofCoins/NumCoins.cpp
@@ -1,7 +1,9 @@
 //here we will decide correct number of coins
 #include <iostream>
+#include <cassert>
 
 int get_change(int m) {
+
 
 	if(m == 0){
 		return 0;
@@ -23,8 +25,46 @@ int get_change(int m) {
 	return minimumCoins;
 }
 
+int get_change_DynamicProgrammng(int monies){
+	//coins: 1, 3, 4
+
+
+	int numOfCoinsForMoney[monies + 1]; //the min number of coins for each value
+
+	numOfCoinsForMoney[0] = 0; //0 value has a min num of coins of 0
+
+	int change_array[3] = {1, 3, 4};
+	int numOfCoins = 0; //keep track of current number of coins
+
+	for(int monies_i = 1; monies_i<monies + 1; ++monies_i){
+		numOfCoinsForMoney[monies_i] = 999999; //just a large number, to get the first min number for this value. 
+
+		for(int change_index = 0; change_index < 3; ++change_index){ //for each type of coin (only 3)
+
+			if(change_array[change_index] <= monies_i){ //make sure you can use this coin
+				numOfCoins = numOfCoinsForMoney[monies_i - change_array[change_index]] + 1;
+			
+				if(numOfCoins < numOfCoinsForMoney[monies_i]){ //since we gotta check each coin, (remember the first time around, it compares it to 999999)
+					numOfCoinsForMoney[monies_i] = numOfCoins;
+				}
+			}
+		}
+	}
+
+	return numOfCoinsForMoney[monies];
+}
+
+void test_solution() {
+    for (int n = 0; n < 10*10; ++n){
+    	assert(get_change_DynamicProgrammng(n) == get_change(n));
+        std::cout<<"Passed for: "<<n<<"\n";
+    }
+}
+
+
 int main() {
 	int m;
 	std::cin >> m;
-	std::cout << get_change(m) << '\n';
+	//test_solution();
+	std::cout << get_change_DynamicProgrammng(m) << '\n';
 }
